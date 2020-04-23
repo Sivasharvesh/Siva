1470da6a436def49a38abcd8b96e102e5c65bcaf
Sivasharvesh <62981976+Sivasharvesh@users.noreply.github.com>
2020-04-23 17:55:24 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# p5.play-boilerplate
-CLASS 20 PROJECT BOILERPLATE
+# SupplyMission
--- sketch.js
+++ sketch.js
@@ -1,43 +1,62 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
 
-var r = 0;
-var g = 50;
-var b=255;
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
 
-var Movingcircle;
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
 
 
+	engine = Engine.create();
+	world = engine.world;
 
-function setup(){
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.3, isStatic:false});
+	World.add(world, packageBody);
+	
 
-  createCanvas(1200,400);
-Movingcircle = createSprite(200,200,50,50);
-shapecolor = "red";
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
 
+
+	Engine.run(engine);
+  
 }
 
 
-function draw(){
+function draw() {
+  rectMode(CENTER);
   background(0);
-
-  ellipseMode(RADIUS);
-
-
-
-  Movingcircle.x = World.mouseX;
-  Movingcircle.y = World.mouseY;
-  Movingcircle.shapecolor = "white";
-
-  if(Movingcircle.x > 150){
-    Movingcircle.shapecolor = "red";
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  drawSprites();
+ 
 }
-if(Movingcircle.x > 200){
-  Movingcircle.shapecolor = "GREEN";
-}
-if(Movingcircle.x > 350){
-  Movingcircle.shapecolor = "BLUE"
-}
-display();
 
- drawSprite();
-
+function KeyPressed(){
+	if(keyCode === DOWN_ARROW){
+		Matter.Body.setStatic(this.body,false);
+	}
 }
+
